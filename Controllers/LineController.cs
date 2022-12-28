6d515216722cb1db140968dc4a9603eca989bf83
chungkai1029 <chungkai1029@gmail.com>
2022-12-28 15:38:20 +0800

    Initialize and create LineController

---
+++ Controllers/LineController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LineBot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LineController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post(dynamic request)
+        {
+            return Ok(request);
+        }
+    }
+}
